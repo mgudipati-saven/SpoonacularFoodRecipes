6a375c261253862fcdf9b67a5a2beb5f6342f700
Murty Gudipati <murty.gudipati@saven.in>
2021-07-07 18:21:03 +0530

    API Key stored in Info.plist

--- FoodRecipes/Model/APIResources.swift
+++ FoodRecipes/Model/APIResources.swift
@@ -22,4 +22,23 @@ struct APIResources {
   static func url(with endpoint: EndPoint) -> URL? {
     urlComponents.url?.appendingPathComponent(endpoint.rawValue)
   }
+
+  static func url(with parameters: [String:String]) -> URL? {
+    var components = URLComponents()
+    components.scheme = "https"
+    components.host = "api.spoonacular.com"
+    components.path = "/recipes/complexSearch"
+    components.queryItems = parameters.map { URLQueryItem(name: $0.key, value: $0.value) }
+
+    let apiKey = Bundle.main.object(forInfoDictionaryKey: "Spoonacular API Key") as? String
+    components.queryItems?.append(URLQueryItem(name: "apiKey", value: apiKey))
+
+    return components.url
+  }
+}
+
+extension URLComponents {
+  mutating func setQueryItems(with parameters: [String:String]) {
+    self.queryItems = parameters.map { URLQueryItem(name: $0.key, value: $0.value) }
+  }
 }
